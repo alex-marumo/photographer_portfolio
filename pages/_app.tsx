f441b25a6bc74ecffcc725bb370abfa3e865d367
alex-marumo <alexmarumo16@gmail.com>
2026-05-23 15:35:33 +0200

    .

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from "next/app";
 import Navigation from "../components/Navigation";
 import "../styles/index.css";
+import { Analytics } from "@vercel/analytics/next";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
